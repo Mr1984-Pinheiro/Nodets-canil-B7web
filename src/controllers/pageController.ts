7d4f042f306e1c89bab1a2254b74985961d3ece7
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-08-11 04:27:26 -0300

    Ativando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -3,6 +3,12 @@ import { Request, Response } from "express";
 export const home = (req: Request, res: Response) => {
     //res.send('Home no controller it is OK!');
         res.render('pages/page', {
+            menu: {
+                all: true,
+                dog: false,
+                cat: false,
+                fish: false
+            },
             banner: {
                 title: 'Todos os animais',
                 background: 'allanimals.jpg'
@@ -12,6 +18,12 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: true,
+            cat: false,
+            fish: false
+        },
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -22,6 +34,12 @@ export const dogs = (req: Request, res: Response) => {
 
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: true,
+            fish: false
+        },
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -32,6 +50,12 @@ export const cats = (req: Request, res: Response) => {
 
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: false,
+            fish: true
+        },
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
